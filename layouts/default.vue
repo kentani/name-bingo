d4e29fc0b266853b22a28f857e837cd51b17f2da
tanikento <kento.tani@seattleconsulting.co.jp>
2021-03-12 06:40:32 +0900

    chore: consoleエラー対応

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,21 +5,6 @@
       clipped
       fixed
       app>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
     </v-navigation-drawer>
     <v-app-bar
       clipped-left
@@ -38,7 +23,7 @@
       </v-container>
     </v-main>
     <v-footer
-      :absolute="!fixed"
+      absolute
       app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
