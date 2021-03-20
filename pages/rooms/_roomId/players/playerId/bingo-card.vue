d64cc08df9117fb642c0921699d172986a55e973
tanikento <kento.tani@seattleconsulting.co.jp>
2021-03-20 19:25:37 +0900

    feat: 参加者がゲーム開始中かどうかを分かるようにする

--- pages/rooms/_roomId/players/playerId/bingo-card.vue
+++ pages/rooms/_roomId/players/playerId/bingo-card.vue
@@ -1,170 +1,184 @@
 <template>
-  <v-row justify="center">
-    <v-col cols="12" sm="9" md="6">
-      <v-card elevation="18">
-        <p
-          class="py-2 my-0 grey lighten-2 display-1 font-weight-bold text-center">
-          BINGO CARD
-        </p>
-        <v-card-actions class="py-0 mx-2">
-          <v-dialog
-            v-model="dialog"
-            scrollable
-            persistent
-            width="900">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                v-bind="attrs"
-                v-on="on"
-                :disabled="isReady || room.isReady"
-                :ripple="false"
-                @click="setSelected">
-                <v-icon color="deep-purple">mdi-account-plus</v-icon>
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title class="font-weight-bold">参加者一覧</v-card-title>
-              <v-card-subtitle v-if="selected.length === 0" class="pb-2">
-                カードに加える人を <span class="title font-weight-bold deep-purple--text">16</span> 人選択してください
-              </v-card-subtitle>
-              <v-card-subtitle v-else-if="selected.length < 16" class="pb-2">
-                あと <span class="title font-weight-bold deep-purple--text">{{ 16 - selected.length }}</span> 人選択してください
-              </v-card-subtitle>
-              <v-card-subtitle v-else-if="selected.length === 16" class="pb-2">
-                <v-icon color="deep-purple">mdi-check</v-icon><span class="title font-weight-bold deep-purple--text"></span>
-              </v-card-subtitle>
-              <v-divider />
-              <v-card-text class="pa-2">
-                <v-card
-                  v-for="(item, i) in joinedList"
-                  :key="i"
-                  class="my-2">
-                  <v-card-title class="title font-weight-bold" :class="[ item.note ? 'pb-1' : 'pb-3' ]">{{ item.name }}</v-card-title>
-                  <v-card-text style="white-space: pre-line;">{{ item.note }}</v-card-text>
-                  <v-card-subtitle class="body-2 font-weight-bold pt-0" :class="[ item.profile ? 'pb-1' : 'pb-3' ]">プロフィール</v-card-subtitle>
-                  <v-card-text style="white-space: pre-line;">{{ item.profile }}</v-card-text>
-                  <v-divider class="mx-4"></v-divider>
-                  <v-card-actions class="py-0 mx-2">
-                    <v-spacer />
-                    <v-switch
-                      v-model="selected"
-                      inset
-                      hide-details
-                      class="my-1"
-                      color="deep-purple"
-                      :disabled="isReady || (!selected.includes(item.id) && selected.length === 16)"
-                      :value="item.id">
-                      <template v-slot:label>
-                        <span
-                          class="overline font-weight-bold">
-                          加える
-                        </span>
-                      </template>
-                    </v-switch>
-                  </v-card-actions>
-                </v-card>
-              </v-card-text>
-              <v-divider />
-              <v-card-actions>
-                <v-spacer />
-                <v-btn
-                  text
-                  rounded
-                  :disabled="selected.length > 16"
-                  @click="resetSelected">
-                  Close
-                </v-btn>
-                <v-btn
-                  rounded
-                  color="deep-purple"
-                  :dark="selected.length <= 16 && !isReady && !room.isReady"
-                  :text="selected.length > 16 || isReady || room.isReady"
-                  :disabled="selected.length > 16 || isReady || room.isReady"
-                  @click="updateSelectList">
-                  Save
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-spacer />
-          <v-switch
-            v-model="isReady"
-            inset
-            hide-details
-            :disabled="selectList.length !== 16 || room.isReady"
-            class="my-2"
-            color="deep-purple">
-            <template v-slot:label>
-              <span
-                class="overline font-weight-bold"
-                :class="[ isReady ? 'deep-purple--text' : 'grey--text' ]">
-                準備完了
-              </span>
-            </template>
-          </v-switch>
-        </v-card-actions>
-        <v-divider class="mx-4" />
-        <v-card-text>
-          <v-row v-if="isReady">
-            <v-col
-              v-for="(item, i) in selectList"
-              :key="i"
-              cols="3"
-              class="pa-1">
-              <v-card
-                :flat="isReady && room.hitList && !room.hitList.includes(item.id)"
-                :class="[ room.hitList && !room.hitList.includes(item.id)? 'grey lighten-4' : 'yellow accent-4' ]"
-                height="100">
-                <v-card-title class="overline pa-1" style="line-height:15px">
-                  {{ item.name }}
-                </v-card-title>
-              </v-card>
-            </v-col>
-          </v-row>
-          <draggable
-            v-else
-            tag="div"
-            class="row"
-            group="cardList"
-            v-model="selected"
-            v-bind="draggableOptions"
-            @start="setSelected"
-            @end="updateSelectList">
-            <v-col
-              v-for="(item, i) in selectList"
-              :key="i"
-              cols="3"
-              class="pa-1">
-              <v-card
-                :flat="isReady && room.hitList && !room.hitList.includes(item.id) || room.isReady"
-                :class="[ room.hitList && !room.hitList.includes(item.id)? 'grey lighten-4' : 'yellow accent-4' ]"
-                height="100">
-                <v-card-title class="overline pa-1" style="line-height:15px">
-                  {{ item.name }}
-                </v-card-title>
-              </v-card>
-            </v-col>
-          </draggable>
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <!-- <v-snackbar
-      v-model="snackbar"
-      :timeout="timeout"
-      centered
-      color="grey">
-      <span class="title font-weight-bold">{{ text }}</span>
-      <template v-slot:action="{ attrs }">
-        <v-btn
+  <div>
+    <v-row justify="center">
+      <v-col cols="12" sm="9" md="6">
+        <v-alert
+          dense
           text
-          v-bind="attrs"
-          @click="snackbar = false">
-          Close
-        </v-btn>
-      </template>
-    </v-snackbar> -->
-  </v-row>
+          elevation="9"
+          :color="setColor()"
+          class="mb-0 text-center">
+          <span class="font-weight-bold">{{ setText() }}</span>
+        </v-alert>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" sm="9" md="6">
+        <v-card elevation="18">
+          <p
+            class="py-2 my-0 grey lighten-2 display-1 font-weight-bold text-center">
+            BINGO CARD
+          </p>
+          <v-card-actions class="py-0 mx-2">
+            <v-dialog
+              v-model="dialog"
+              scrollable
+              persistent
+              width="900">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  v-bind="attrs"
+                  v-on="on"
+                  :disabled="isReady || room.isReady"
+                  :ripple="false"
+                  @click="setSelected">
+                  <v-icon color="deep-purple">mdi-account-plus</v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="font-weight-bold">参加者一覧</v-card-title>
+                <v-card-subtitle v-if="selected.length === 0" class="pb-2">
+                  カードに加える人を <span class="title font-weight-bold deep-purple--text">16</span> 人選択してください
+                </v-card-subtitle>
+                <v-card-subtitle v-else-if="selected.length < 16" class="pb-2">
+                  あと <span class="title font-weight-bold deep-purple--text">{{ 16 - selected.length }}</span> 人選択してください
+                </v-card-subtitle>
+                <v-card-subtitle v-else-if="selected.length === 16" class="pb-2">
+                  <v-icon color="deep-purple">mdi-check</v-icon><span class="title font-weight-bold deep-purple--text"></span>
+                </v-card-subtitle>
+                <v-divider />
+                <v-card-text class="pa-2">
+                  <v-card
+                    v-for="(item, i) in joinedList"
+                    :key="i"
+                    class="my-2">
+                    <v-card-title class="title font-weight-bold" :class="[ item.note ? 'pb-1' : 'pb-3' ]">{{ item.name }}</v-card-title>
+                    <v-card-text style="white-space: pre-line;">{{ item.note }}</v-card-text>
+                    <v-card-subtitle class="body-2 font-weight-bold pt-0" :class="[ item.profile ? 'pb-1' : 'pb-3' ]">プロフィール</v-card-subtitle>
+                    <v-card-text style="white-space: pre-line;">{{ item.profile }}</v-card-text>
+                    <v-divider class="mx-4"></v-divider>
+                    <v-card-actions class="py-0 mx-2">
+                      <v-spacer />
+                      <v-switch
+                        v-model="selected"
+                        inset
+                        hide-details
+                        class="my-1"
+                        color="deep-purple"
+                        :disabled="isReady || (!selected.includes(item.id) && selected.length === 16)"
+                        :value="item.id">
+                        <template v-slot:label>
+                          <span
+                            class="overline font-weight-bold">
+                            加える
+                          </span>
+                        </template>
+                      </v-switch>
+                    </v-card-actions>
+                  </v-card>
+                </v-card-text>
+                <v-divider />
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn
+                    text
+                    rounded
+                    :disabled="selected.length > 16"
+                    @click="resetSelected">
+                    Close
+                  </v-btn>
+                  <v-btn
+                    rounded
+                    color="deep-purple"
+                    :dark="selected.length <= 16 && !isReady && !room.isReady"
+                    :text="selected.length > 16 || isReady || room.isReady"
+                    :disabled="selected.length > 16 || isReady || room.isReady"
+                    @click="updateSelectList">
+                    Save
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-spacer />
+            <v-switch
+              v-model="isReady"
+              inset
+              hide-details
+              :disabled="selectList.length !== 16 || room.isReady"
+              class="my-2"
+              color="deep-purple">
+              <template v-slot:label>
+                <span
+                  class="overline font-weight-bold"
+                  :class="[ isReady ? 'deep-purple--text' : 'grey--text' ]">
+                  準備完了
+                </span>
+              </template>
+            </v-switch>
+          </v-card-actions>
+          <v-divider class="mx-4" />
+          <v-card-text>
+            <v-row v-if="isReady">
+              <v-col
+                v-for="(item, i) in selectList"
+                :key="i"
+                cols="3"
+                class="pa-1">
+                <v-card
+                  :flat="isReady && room.hitList && !room.hitList.includes(item.id)"
+                  :class="[ room.hitList && !room.hitList.includes(item.id)? 'grey lighten-4' : 'yellow accent-4' ]"
+                  height="100">
+                  <v-card-title class="overline pa-1" style="line-height:15px">
+                    {{ item.name }}
+                  </v-card-title>
+                </v-card>
+              </v-col>
+            </v-row>
+            <draggable
+              v-else
+              tag="div"
+              class="row"
+              group="cardList"
+              v-model="selected"
+              v-bind="draggableOptions"
+              @start="setSelected"
+              @end="updateSelectList">
+              <v-col
+                v-for="(item, i) in selectList"
+                :key="i"
+                cols="3"
+                class="pa-1">
+                <v-card
+                  :flat="isReady && room.hitList && !room.hitList.includes(item.id) || room.isReady"
+                  :class="[ room.hitList && !room.hitList.includes(item.id)? 'grey lighten-4' : 'yellow accent-4' ]"
+                  height="100">
+                  <v-card-title class="overline pa-1" style="line-height:15px">
+                    {{ item.name }}
+                  </v-card-title>
+                </v-card>
+              </v-col>
+            </draggable>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <!-- <v-snackbar
+        v-model="snackbar"
+        :timeout="timeout"
+        centered
+        color="grey">
+        <span class="title font-weight-bold">{{ text }}</span>
+        <template v-slot:action="{ attrs }">
+          <v-btn
+            text
+            v-bind="attrs"
+            @click="snackbar = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar> -->
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -247,6 +261,24 @@
       },
       changeStatus(status) {
         this.$store.dispatch('updateReady', { status: status, playerId: this.playerId })
+      },
+      setColor() {
+        if (this.room.isReady) {
+          return 'deep-purple'
+        } else if (this.player.isReady) {
+          return 'grey'
+        } else {
+          return 'red darken-4'
+        }
+      },
+      setText() {
+        if (this.room.isReady) {
+          return 'ビンゴ開催中'
+        } else if (this.player.isReady) {
+          return 'ビンゴの開催を待ってください'
+        } else {
+          return 'カードの準備をしてください'
+        }
       }
     }
   }
